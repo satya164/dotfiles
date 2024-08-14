c996921bc739c66f0c109dce789245e1cfe91bb8
Satyajit Sahoo <satyajit.happy@gmail.com>
2024-08-14 05:50:26 +0200

    Handle personal macbook in flake

--- .config/nix-darwin/flake.nix
+++ .config/nix-darwin/flake.nix
@@ -39,7 +39,9 @@
 
       homebrew = {
         enable = true;
-        taps = [];
+        taps = [
+          "nikitabobko/tap"
+        ];
         brews = [];
         casks = [
           "aerospace"
@@ -48,19 +50,14 @@
         ];
       };
 
-      services.nix-daemon.enable = true;
-
-      nix.settings.experimental-features = "nix-command flakes";
-
       programs.zsh.enable = true;
 
       security.pam.enableSudoTouchIdAuth = true;
 
+      services.nix-daemon.enable = true;
+
       system.configurationRevision = self.rev or self.dirtyRev or null;
       system.stateVersion = 4;
-
-      nixpkgs.hostPlatform = "aarch64-darwin";
-
       system.defaults = {
         dock.autohide = true;
         dock.mru-spaces = false;
@@ -70,7 +67,13 @@
     };
   in
   {
+    darwinConfigurations.macbook = nix-darwin.lib.darwinSystem {
+      system = "x86_64-darwin";
+      modules = [ configuration ];
+    };
+
     darwinConfigurations."INV-0068" = nix-darwin.lib.darwinSystem {
+      system = "aarch64-darwin";
       modules = [ configuration ];
     };
   };
