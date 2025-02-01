8f76d90e0064c3515347765381ff19a213c705db
Satyajit Sahoo <satyajit.happy@gmail.com>
2025-02-01 02:59:10 +0100

    Update nix-darwin

--- .nix/darwin/flake.nix
+++ .nix/darwin/flake.nix
@@ -2,7 +2,7 @@
   description = "satya164's macOS flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.05-darwin";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.11-darwin";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
   };
