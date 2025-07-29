8ac3b1e68ddc77020955f151194c671be82ca083
Satyajit Sahoo <satyajit.happy@gmail.com>
2025-07-29 21:17:32 +0200

    Enable avahi for all linux hosts

--- .nix/common/linux.nix
+++ .nix/common/linux.nix
@@ -54,6 +54,19 @@ in
     };
   };
 
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    publish = {
+      enable = true;
+      addresses = true;
+      domain = true;
+      hinfo = true;
+      userServices = true;
+      workstation = true;
+    };
+  };
+
   services.tailscale.enable = true;
 
   services.journald = {
--- .nix/hosts/homelab/configuration.nix
+++ .nix/hosts/homelab/configuration.nix
@@ -140,19 +140,6 @@ in
     openFirewall = true;
   };
 
-  services.avahi = {
-    enable = true;
-    nssmdns4 = true;
-    publish = {
-      enable = true;
-      addresses = true;
-      domain = true;
-      hinfo = true;
-      userServices = true;
-      workstation = true;
-    };
-  };
-
   virtualisation.docker = {
     enable = true;
     daemon.settings = {
